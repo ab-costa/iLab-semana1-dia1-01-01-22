9ca710f23d84cff495625367e56761c44a138fd1
ab-costa <costa.alessandroborges@gmail.com>
2022-02-04 21:55:08 -0300

    Solution uri 1003

---
+++ Uri1003.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Uri1003 {
+    public static void main(String args[]) {
+        Scanner teclado = new Scanner(System.in);
+
+        int a, b, soma;
+
+        a = teclado.nextInt();
+        b = teclado.nextInt();
+
+        soma = a + b;
+
+        System.out.printf("SOMA = " + soma);
+
+        teclado.close();
+    }
+}
