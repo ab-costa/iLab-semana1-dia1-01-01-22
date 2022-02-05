128c9460c90082be2ca2a416982a5257ff5729fc
ab-costa <costa.alessandroborges@gmail.com>
2022-02-05 00:03:23 -0300

    Solution uri 1012

---
+++ Uri1012.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Uri1012 {
+    public static void main(String args[]) {
+        Scanner teclado = new Scanner(System.in);
+
+        double a, b, c;
+
+        a = teclado.nextDouble();
+        b = teclado.nextDouble();
+        c = teclado.nextDouble();
+
+        System.out.printf("TRIANGULO: %.3f\n", (a * c) / 2);
+        System.out.printf("CIRCULO: %.3f\n", 3.14159 * c * c);
+        System.out.printf("TRAPEZIO: %.3f\n", ((a + b) * c) / 2);
+        System.out.printf("QUADRADO: %.3f\n", b * b);
+        System.out.printf("RETANGULO: %.3f\n", a * b);
+
+        teclado.close();
+    }
+}
